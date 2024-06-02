5da52e4dc89d1edf957739c73ca86065ae261770
StephenDelaCruz <133613262+StephenDelaCruz@users.noreply.github.com>
2024-06-02 15:29:12 +1200

    Add files via upload

---
+++ ProductModel.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication8;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mcste
+ */
+public class ProductModel {
+
+    private static final String URL = "jdbc:derby:PearStoreDB_Ebd";
+
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM PRODUCTS";
+
+        try ( Connection conn = DriverManager.getConnection(URL, "pdc", "pdc");
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("PRODUCTID");
+                String item = rs.getString("ITEM");
+                double price = rs.getDouble("PRICE");
+                int stock = rs.getInt("STOCK");
+
+
+                    products.add(new Product(id, item, price, stock));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
+}
