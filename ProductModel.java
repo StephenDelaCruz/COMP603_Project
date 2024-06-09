2cbdbeb9d608e94db4678a5e0e53f33207f3e099
StephenDelaCruz <133613262+StephenDelaCruz@users.noreply.github.com>
2024-06-09 17:17:58 +1200

    Update ProductModel.java

--- ProductModel.java
+++ ProductModel.java
@@ -10,29 +10,28 @@ import java.util.List;
 
 /**
  *
- * @author mcste
+ * @author neill
  */
 public class ProductModel {
 
     private static final String URL = "jdbc:derby:PearStoreDB_Ebd";
 
-public List<Product> getAllProducts() {
-    List<Product> products = new ArrayList<>();
-    String query = "SELECT * FROM PRODUCTS";
-    try (Connection conn = DriverManager.getConnection(URL, "pdc", "pdc");
-         Statement stmt = conn.createStatement();
-         ResultSet rs = stmt.executeQuery(query)) {
-        while (rs.next()) {
-            int id = rs.getInt("PRODUCTID");
-            String item = rs.getString("ITEM");
-            double price = rs.getDouble("PRICE");
-            int stock = rs.getInt("STOCK");
-            int warranty = rs.getInt("WARRANTY");  
-            products.add(new Product(id, item, price, stock, warranty));
+    //retrives our products from our database, so that it can be used for the display
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM PRODUCTS";
+        try ( Connection conn = DriverManager.getConnection(URL, "pdc", "pdc");  Statement stmt = conn.createStatement();  ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                int id = rs.getInt("PRODUCTID");
+                String item = rs.getString("ITEM");
+                double price = rs.getDouble("PRICE");
+                int stock = rs.getInt("STOCK");
+                int warranty = rs.getInt("WARRANTY");
+                products.add(new Product(id, item, price, stock, warranty));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    } catch (SQLException e) {
-        e.printStackTrace();
+        return products;
     }
-    return products;
-}
 }
